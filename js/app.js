c471f4c23b022223311f02d0ceeecea625a0f287
kr8tivitco <knmckinney@Kristons-MacBook-Pro.local>
2014-07-09 11:50:50 -0400

    quantity commit
    Added quantity to the bottom of app. Keeps track of how many items have
    been checked off

--- js/app.js
+++ js/app.js
@@ -1,101 +1,86 @@
+
+
 $(document).ready(function() {
 
   $('h6').click(function() {
-    $('.directions-contain').animate({width: 'toggle'});
-    
+    $('.directions-contain').slideToggle();
   });
-
-
 
   //var item = $('#listItem').val();
-    var itemNum = 0;
-    
-  $('#addList').click(function(e){
-        e.preventDefault();
-    
-      
+  var itemNum = 0;
+  var checkNum =0;
 
+  $('#addList').click(function(e) {
+      e.preventDefault();
       var listItem = $('#listItem').val();
+    if (itemNum == 10) { alert('You have reached the list limit'); 
+    }  
 
-        if (itemNum ==10) {
-            alert('You have reached the list limit');
-        }
-          
-          /*else if ($('#listItem').val() == " ") {
-            alert('Please Input List Item');
-          
-          }*/
-          
-          else {
-            itemNum++;
-      // $('.currentList').append('<input type="checkbox" name="chkBox" >' + listItem + '<input type="submit" name="delBtn" >');  // WORKS ADDS AS TEXT
-           // $("#shopList ul").append('<li>' + '<input type="checkbox" name="chkBox" id="chkBox" >' +listItem+ '<input type="submit" id="del" name="delBtn" value="X" >' + '</li>'); //WORKS ADDS AS LI w/ checkbox & delete button
-              $("#shopList ul").append('<li class="appendLiItem">' + '<input type="checkbox" name="chkBox" id="chkBox" >' +listItem+ '</li>');
+    else {
+      itemNum++;
 
+// blanche
+      $("#shopList ul").append('<li class="appendLiItem"> <input type="checkbox" name="chkBox" id="chkBox" class="delete" >' + listItem + '</li>');
 
-            $('#listItem').val(" ");
-          }        
+      $(".chkQty").html("Items Left: " + (itemNum-checkNum) + " of 10");
+      //$('#listItem').val(" ");  // reset input field
+
+    }
   });
-            
-            //////////////CAPABILITY YOU WANT TO ADD//////////////////////////
-            //1. IF ITEM CHECK OUTLINE LI CHANGES FROM WHITE TO ANOTHER COLOR    100% COMPLETE
-            //2. GET REQUIRED ATTR TO WORK
-            //3. ADD HOW MANY LIST ITEMS
-            //4. ADD DIRECTIONS THAT SLIDE IN                                     100% COMPLETE
+
+  //////////////CAPABILITY YOU WANT TO ADD//////////////////////////
+  //1. IF ITEM CHECK OUTLINE LI CHANGES FROM WHITE TO ANOTHER COLOR    100% COMPLETE
+  //2. GET REQUIRED ATTR TO WORK
+  //3. ADD HOW MANY LIST items
+  //4. ADD DIRECTIONS THAT SLIDE IN                                     100% COMPLETE
 
 
-            //On Delete remove LI entry Kriston
-      /*$("#shopList ul").on('click','input[type=submit]' , function(el){
-            
-               $(this).parent().remove();
-               
-               itemNum--;
-      });*/
+// blanche
+  
+          $("#shopList ul").on('click','.delete' , function(e){
+              $(this).parent().remove();               
+  
+          });
 
-    //removes list items
-    
-    /*$('#shopList ul').on('click', '.delete', function(e){
 
-      e.preventDefault(); 
+  //crosses off list items Kriston
+  $("#shopList ul").on('click', 'input[type=checkbox]', function() {
 
-        $(this).parent().remove();
+    var itemCheckED = $(this).parent();
 
-                itemNum--;
-      }); */
+    if ($(this).is(':checked')) {
+      $(itemCheckED).css('textDecoration', 'line-through');
+      $(itemCheckED).css('border-top', '2px solid #4A96AD');
+      $(itemCheckED).css('border-bottom', '2px solid #4A96AD');
+      $(itemCheckED).css('border-left', '4px solid #4A96AD');
+      $(itemCheckED).css('border-right  ', '4px solid #4A96AD ');
+      checkNum++;
+      $(".chkQty").html("Items Left: " + (itemNum-checkNum) + " of 10");
 
- 
-    //crosses off list items Kriston
-    $("#shopList ul").on('click','input[type=checkbox]' , function(){
-            
-      var itemCheckED = $(this).parent();
-                
-          if ($(this).is(':checked') ) {
-                $(itemCheckED).css('textDecoration','line-through');
-                $(itemCheckED).css('border-top', '2px solid #4A96AD');
-                $(itemCheckED).css('border-bottom', '2px solid #4A96AD');
-                $(itemCheckED).css('border-left', '4px solid #4A96AD');
-                $(itemCheckED).css('border-right  ', '4px solid #4A96AD ');  
+        
 
-          }
-          else {
-                $(itemCheckED).css('textDecoration','none');
-                $(itemCheckED).css('border-top', '2px solid white');
-                $(itemCheckED).css('border-bottom', '2px solid white');
-                $(itemCheckED).css('border-left', '4px solid white');
-                $(itemCheckED).css('border-right  ', '4px solid white');
-                
-          }
-            
-    });
+    //} //else if (($(this).is(':checked')) && ($("#shopList ul").on('click','.trashcanBtn')) {
+              //$(itemCheckED).remove();   
 
-        //$('.trashcan-contain').on('click', '#trashcanBtn', function(e){
-            
-            //var checkedItemDel = $(this).closest('#shopList').find('#chkBox:checked').parent();
-            //$('##chkBox').is(':checked');
-              //checkedItemDel.hide();
+    } else {
+      $(itemCheckED).css('textDecoration', 'none');
+      $(itemCheckED).css('border-top', '2px solid white');
+      $(itemCheckED).css('border-bottom', '2px solid white');
+      $(itemCheckED).css('border-left', '4px solid white');
+      $(itemCheckED).css('border-right  ', '4px solid white');
+      checkNum--;
+      $(".chkQty").html("Items Left: " + (itemNum-checkNum) + " of 10");
 
-       // });
+    }
 
+  });
+
+/*
+  $('.trashcan-contain').on('click', '#trashcanBtn', function(e){
+    var checkedItemDel = $(this).closest('#shopList').find('#chkBox:checked').parent();
+    $('##chkBox').is(':checked');
+    checkedItemDel.hide();
+  });
+*/
 
 });
-
